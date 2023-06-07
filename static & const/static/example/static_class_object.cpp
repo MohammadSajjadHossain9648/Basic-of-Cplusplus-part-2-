e3bbc71327823ff373dc2a8756981e5ee0e0cfb1
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-06-07 20:31:32 +0600

    static class object

--- static & const/static/example/static_class_object.cpp
+++ static & const/static/example/static_class_object.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
 using namespace std;
 /*
+    to know more: https://www.geeksforgeeks.org/static-variables-in-c/
+                  https://www.geeksforgeeks.org/static-keyword-cpp/
+                  https://www.geeksforgeeks.org/memory-layout-of-c-program/
+                  https://www.geeksforgeeks.org/what-are-static-functions-in-c/
+                  https://dotnettutorials.net/lesson/static-variables-in-cpp
+                  https://linuxhint.com/static-variable-cpp/
+                  https://www.learncpp.com/cpp-tutorial/static-local-variables/
+                  https://www.cs.uaf.edu/2010/spring/cs202/lecture/02_16_static.html
+                  https://www.scaler.com/topics/static-variables-in-c/
+                  https://www.scaler.com/topics/cpp/static-member-in-cpp/
+                  https://www.programiz.com/cpp-programming/storage-class
+                  https://www.softwaretestinghelp.com/static-in-cpp/
+                  https://www.studytonight.com/cpp/static-keyword.php
+                  https://www.javatpoint.com/cpp-static
+                  https://www.tutorialspoint.com/static-keyword-in-cplusplus
+*/
+/*
     Just like static member variables of class, we can declare class objects as static. Static class objects are
     also initialized only once and remain active throughout the program. As the object is a user-defined type, a
     static class object is initialized similarly to the ordinary objects using a constructor.
