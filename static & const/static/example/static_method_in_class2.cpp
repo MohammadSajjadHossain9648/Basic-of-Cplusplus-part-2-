ced26d2fd93021f686f352f8d4ca348c49af54ba
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-06-08 20:52:47 +0600

    static method in class

--- static & const/static/example/static_method_in_class2.cpp
+++ static & const/static/example/static_method_in_class2.cpp
@@ -1,6 +1,22 @@
 #include<iostream>
 using namespace std;
-
+/*
+    to know more: https://www.geeksforgeeks.org/static-variables-in-c/
+                  https://www.geeksforgeeks.org/static-keyword-cpp/
+                  https://www.geeksforgeeks.org/memory-layout-of-c-program/
+                  https://www.geeksforgeeks.org/what-are-static-functions-in-c/
+                  https://dotnettutorials.net/lesson/static-variables-in-cpp
+                  https://linuxhint.com/static-variable-cpp/
+                  https://www.learncpp.com/cpp-tutorial/static-local-variables/
+                  https://www.cs.uaf.edu/2010/spring/cs202/lecture/02_16_static.html
+                  https://www.scaler.com/topics/static-variables-in-c/
+                  https://www.scaler.com/topics/cpp/static-member-in-cpp/
+                  https://www.programiz.com/cpp-programming/storage-class
+                  https://www.softwaretestinghelp.com/static-in-cpp/
+                  https://www.studytonight.com/cpp/static-keyword.php
+                  https://www.javatpoint.com/cpp-static
+                  https://www.tutorialspoint.com/static-keyword-in-cplusplus
+*/
 class sample
 {
     int var;
