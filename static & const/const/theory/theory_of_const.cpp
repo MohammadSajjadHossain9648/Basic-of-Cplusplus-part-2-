58dd5d0fc7c4c5ef6b55edae7f8a6281b4ea4485
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-06-12 20:26:40 +0600

    theory of const

---
+++ static & const/const/theory/theory_of_const.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/const-keyword-in-cpp/
+                  https://www.javatpoint.com/const-keyword-in-cpp
+                  https://learn.microsoft.com/en-us/cpp/cpp/const-cpp?view=msvc-170
+                  https://www.studytonight.com/cpp/const-keyword.php
+                  https://www.freecodecamp.org/news/constants-in-c-explained-how-to-use-define-and-const-keyword/
+                  https://www.learncpp.com/cpp-tutorial/const-variables-and-symbolic-constants/
+                  https://eecs280staff.github.io/notes/06_Compound_Objects.html
+                  https://www.udacity.com/blog/2021/08/developers-guide-to-cpp-constant-variables.html
+                  https://www.codesdope.com/cpp-const-keyword/
+                  https://www.possibility.com/Cpp/const.html
+*/
+/*
+    It is a const variable used to define the variable values that never be changed during the execution of a program.
+    And if we try to modify the value, it throws an error.
+*/
+/*
+    Constant is something that doesn't change. In C language and C++ we use the keyword const to make program
+    elements constant.
+
+    There are a certain set of rules for the declaration and initialization of the constant variables:
+    1.The const variable cannot be left un-initialized at the time of the assignment.
+    2.It cannot be assigned value anywhere in the program.
+    3.Explicit value needed to be provided to the constant variable at the time of declaration of the
+    constant variable.
+*/
+/*
+    Use const keywords with different parameters:
+
+    1.Use const variable
+    2.Use const with pointers
+    3.Use const pointer with variables
+    4.Use const with function arguments
+    5.Use const with class member functions
+    6.Use const with class data members
+    7.Use const with class objects
+*/
+/*
+    What is a symbolic constant?
+    A symbolic constant is a name that is given to a constant value. Constant variables are one type of symbolic
+    constant, as a variable has a name (its identifier) and a constant value.
+
+    for example:
+        # define PI 3.14
+*/
+int main()
+{
+}
