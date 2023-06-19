470f5445177775a1b39f1561cd75af7660ed625d
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-06-19 21:26:01 +0600

    const as function return type

--- static & const/const/example/const_as_function_return_type.cpp
+++ static & const/const/example/const_as_function_return_type.cpp
@@ -1,5 +1,17 @@
 #include<iostream>
 using namespace std;
+/*
+    to know more: https://www.geeksforgeeks.org/const-keyword-in-cpp/
+                  https://www.javatpoint.com/const-keyword-in-cpp
+                  https://learn.microsoft.com/en-us/cpp/cpp/const-cpp?view=msvc-170
+                  https://www.studytonight.com/cpp/const-keyword.php
+                  https://www.freecodecamp.org/news/constants-in-c-explained-how-to-use-define-and-const-keyword/
+                  https://www.learncpp.com/cpp-tutorial/const-variables-and-symbolic-constants/
+                  https://eecs280staff.github.io/notes/06_Compound_Objects.html
+                  https://www.udacity.com/blog/2021/08/developers-guide-to-cpp-constant-variables.html
+                  https://www.codesdope.com/cpp-const-keyword/
+                  https://www.possibility.com/Cpp/const.html
+*/
 const int foo(int y)
 {
     y--;
