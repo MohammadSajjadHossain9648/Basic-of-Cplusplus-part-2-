44beaf32c7df729ddfb9ae7c71b22d563c1c05fb
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-07-03 19:28:17 +0600

    double pointer(Pointers to pointers)

--- pointer & reference/Pointer/example/basic/double_pointer(Pointers to pointers).cpp
+++ pointer & reference/Pointer/example/basic/double_pointer(Pointers to pointers).cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 /*
+    to know more: https://www.geeksforgeeks.org/cpp-pointers/#:~:text=Pointers%20are%20symbolic%20representations%20of,the%20main%20use%20of%20pointers.
+                  https://www.guru99.com/cpp-pointers.html
+                  https://www.simplilearn.com/tutorials/cpp-tutorial/pointers-in-cpp
+                  https://www.scaler.com/topics/cpp/pointers-in-cpp/
+                  https://www.softwaretestinghelp.com/pointers-in-cpp/
+                  https://www.javatpoint.com/cpp-pointers
+                  https://cplusplus.com/doc/tutorial/pointers/
+                  https://www.programiz.com/cpp-programming/pointers
+                  https://www.tutorialspoint.com/cplusplus/cpp_pointers.htm
+*/
+/*
     Pointers to pointers(double pointer)
     C++ allows the use of pointers that point to pointers, that these, in its turn, point to data (or even to other
     pointers). The syntax simply requires an asterisk (*) for each level of indirection in the declaration
