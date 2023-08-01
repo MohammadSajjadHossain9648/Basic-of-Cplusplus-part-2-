16b8029346fb3f4109308fc941a263e150dd5c06
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-08-01 21:22:31 +0600

    initialize a class(basic)

--- Object-Oriented Programming/Object-Oriented Programming basic/basic/example/initialize_a_class(basic).cpp
+++ Object-Oriented Programming/Object-Oriented Programming basic/basic/example/initialize_a_class(basic).cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
 /*
+    to know more: https://www.simplilearn.com/tutorials/cpp-tutorial/oops-concepts-in-cpp
+                  https://www.geeksforgeeks.org/object-oriented-programming-in-cpp/
+                  https://www.freecodecamp.org/news/object-oriented-programming-in-c/
+                  https://www.javatpoint.com/cpp-oops-concepts
+                  https://www.programiz.com/cpp-programming/object-class
+                  https://www.educative.io/blog/is-cpp-object-oriented-programming-language
+                  https://www.techtarget.com/searchapparchitecture/definition/object-oriented-programming-OOP
+                  https://www.indeed.com/career-advice/career-development/what-is-object-oriented-programming
+                  https://www.codecademy.com/learn/c-plus-plus-for-programmers/modules/object-oriented-programming-in-cpp/cheatsheet
+                  https://levelup.gitconnected.com/cpp-object-oriented-programming-an-in-depth-guide-f56a597091d8
+*/
+/*
     object: An object is an entity that has state/properties and behavior. or can say that an object is an instance
     of a class. object is a template or blueprint of a class.
     like an example,
