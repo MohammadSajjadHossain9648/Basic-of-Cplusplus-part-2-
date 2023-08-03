f0eaaeb2392467080c4305190feb9effc589f7ae
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-08-03 19:30:54 +0600

    access data members and member functions

---
+++ Object-Oriented Programming/Object-Oriented Programming basic/basic/example/access_data_members_and_member_functions.cpp
@@ -0,0 +1,178 @@
+#include<iostream>
+using namespace std;
+/*
+    data members/attributes are class variable and member functions are class function
+*/
+class Man
+{
+    string name; //private data member without declare access modifier(private)
+};
+
+class PI
+{
+public:
+    double pi = 3.1416; //public data member
+
+    int printPi() //public member function
+    {
+        cout<<"\nPI value in class: "<<pi;
+    }
+};
+
+
+class Student
+{
+public:
+    //data member(also instance variable)
+    int id;
+    string name;
+
+    //member function to initialize value
+    void setValue(int i, string n)
+    {
+        id = i;
+        name = n;
+    }
+
+    //member function
+    void display()
+    {
+        cout<<"\n"<<id<<"  "<<name;
+    }
+};
+
+class Teacher
+{
+public:
+    //data member(also instance variable)
+    int id;
+    string name;
+
+    //constructor
+    Teacher(int i, string n)
+    {
+        id = i;
+        name = n;
+    }
+
+    //member function
+    void display()
+    {
+        cout<<"\n"<<id<<"  "<<name;
+    }
+};
+
+
+class Room
+{
+public:
+    //data member
+    double length;
+    double breadth;
+    double height;
+
+    //member function
+    double calculateArea()
+    {
+        return length * breadth;
+    }
+
+    double calculateVolume()
+    {
+        return length * breadth * height;
+    }
+};
+
+
+int main()
+{
+    //object create of class Man
+    Man m;
+
+//another example
+    //object create of class PI
+    PI a;
+
+    //access data member which is a.pi
+    cout<<"\nPI value in main(): "<<a.pi;
+
+    //access member function
+    a.printPi();
+
+
+    //output:
+    //
+    //    PI value in main(): 3.1416
+    //    PI value in class: 3.1416
+
+
+
+
+//another example
+    Student s1;
+    Student s2;
+
+    s1.setValue(201, "asif");
+    s2.setValue(202, "faisal");
+
+    /* without setValue function
+    s1.id = 201;
+    s1.name = "asif";
+
+    s2.id = 202;
+    s2.name = "faisal";
+    */
+
+    s1.display();
+    s2.display();
+
+    //output:
+    //    201  asif
+    //    202  faisal
+
+
+
+
+//another example: initialize value using constructor
+    Teacher t(1005, "tamim");
+    t.display();
+
+
+    //output: 1005  tamim
+
+
+
+// another example: assign values to data members
+    // create object of Room class
+    Room r1;
+
+    // assign values to data members
+    r1.length = 42.5;
+    r1.breadth = 30.8;
+    r1.height = 19.2;
+
+    cout<<"\n\nArea of Room =  "<<r1.calculateArea();
+    // calculate and display the area and volume of the room
+    cout<<"\nVolume of Room =  "<<r1.calculateVolume();
+
+
+
+    //after changing r1 object length
+    cout<<"\n\nAfter changing r1 length: ";
+    r1.length = 50;
+    cout<<"\n\nArea of Room =  "<<r1.calculateArea();
+    // calculate and display the area and volume of the room
+    cout<<"\nVolume of Room =  "<<r1.calculateVolume();
+
+
+    //output:
+    //
+    //    Area of Room =  1309
+    //    Volume of Room =  25132.8
+    //
+    //
+    //    After changing r1 length:
+    //
+    //    Area of Room =  1540
+    //    Volume of Room =  29568
+}
