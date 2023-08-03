7faee04cb0afd05e6ce68a8a9a97128d0b35f4fa
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-08-03 19:31:37 +0600

    access data members and member functions(original)

---
+++ Object-Oriented Programming/Object-Oriented Programming basic/basic/example/access_data_members_and_member_functions_2(original).cpp
@@ -0,0 +1,187 @@
+#include<iostream>
+using namespace std;
+/*
+    data members/attributes are class variable and member functions are class function
+*/
+class Room
+{
+public:
+    //data member
+    double length;
+    double breadth;
+    double height;
+
+    //member function
+    double calculateArea()
+    {
+        return length * breadth;
+    }
+
+    double calculateVolume()
+    {
+        return length * breadth * height;
+    }
+};
+
+class Room2
+{
+public:
+    //data member
+    double length;
+    double breadth;
+    double height;
+
+    Room2(double l, double b, double h)
+    {
+        length = l;
+        breadth = b;
+        height = h;
+    }
+
+    //member function
+    double calculateArea()
+    {
+        return length * breadth;
+    }
+
+    double calculateVolume()
+    {
+        return length * breadth * height;
+    }
+};
+
+class Room3
+{
+private:
+    //data member
+    double length;
+    double breadth;
+    double height;
+
+public:
+    Room3(double l, double b, double h)
+    {
+        length = l;
+        breadth = b;
+        height = h;
+    }
+
+    void setLength(int l)
+    {
+        length = l;
+    }
+    int getLength()
+    {
+        return length;
+    }
+
+    //member function
+    double calculateArea()
+    {
+        return length * breadth;
+    }
+
+    double calculateVolume()
+    {
+        return length * breadth * height;
+    }
+};
+
+
+int main()
+{
+//assign values to data members
+    // create object of Room class
+    Room r1;
+
+    // assign values to data members
+    r1.length = 42.5;
+    r1.breadth = 30.8;
+    r1.height = 19.2;
+
+    cout<<"\n\nArea of Room =  "<<r1.calculateArea();
+    // calculate and display the area and volume of the room
+    cout<<"\nVolume of Room =  "<<r1.calculateVolume();
+
+
+
+    //after changing r1 object length
+    cout<<"\n\nAfter changing r1 length: ";
+    r1.length = 50;
+    cout<<"\n\nArea of Room =  "<<r1.calculateArea();
+    // calculate and display the area and volume of the room
+    cout<<"\nVolume of Room =  "<<r1.calculateVolume();
+
+
+    //output:
+    //
+    //    Area of Room =  1309
+    //    Volume of Room =  25132.8
+    //
+    //
+    //    After changing r1 length:
+    //
+    //    Area of Room =  1540
+    //    Volume of Room =  29568
+
+
+
+// another above same example: assign values to data members with using constructor
+    // create object of Room class
+    Room2 r2(42.5, 30.8, 19.2);
+
+    cout<<"\n\nArea of Room =  "<<r2.calculateArea();
+    // calculate and display the area and volume of the room
+    cout<<"\nVolume of Room =  "<<r2.calculateVolume();
+
+
+
+    //after changing r1 object length
+    cout<<"\n\nAfter changing r2 length: ";
+    r2.length = 50;
+    cout<<"\n\nArea of Room =  "<<r2.calculateArea();
+    // calculate and display the area and volume of the room
+    cout<<"\nVolume of Room =  "<<r2.calculateVolume();
+
+
+    //output:
+    //
+    //    Area of Room =  1309
+    //    Volume of Room =  25132.8
+    //
+    //
+    //    After changing r2 length:
+    //
+    //    Area of Room =  1540
+    //    Volume of Room =  29568
+
+
+// another above same example: assign values to data members with using constructor
+    // create object of Room class
+    Room3 r3(42.5, 30.8, 19.2);
+
+    cout<<"\n\nArea of Room =  "<<r3.calculateArea();
+    // calculate and display the area and volume of the room
+    cout<<"\nVolume of Room =  "<<r3.calculateVolume();
+
+
+
+    //after changing r1 object length
+    cout<<"\n\nAfter changing r3 length: ";
+    r3.setLength(50);   //r3.length = 50; ->show error
+
+    cout<<"\n\nArea of Room =  "<<r3.calculateArea();
+    // calculate and display the area and volume of the room
+    cout<<"\nVolume of Room =  "<<r3.calculateVolume();
+
+    //output:
+    //
+    //    Area of Room =  1309
+    //    Volume of Room =  25132.8
+    //
+    //
+    //    After changing r3 length:
+    //
+    //    Area of Room =  1540
+    //    Volume of Room =  29568
+}
