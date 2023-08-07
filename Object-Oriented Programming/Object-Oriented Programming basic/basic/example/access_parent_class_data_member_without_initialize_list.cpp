c932c8cea474167841da3657b7a20d4a197fe8c1
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-08-07 23:24:24 +0600

    access parent class data member without initialize list

---
+++ Object-Oriented Programming/Object-Oriented Programming basic/basic/example/access_parent_class_data_member_without_initialize_list.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+/*
+    before start read this two files -> access modifiers with inheritance / basic
+*/
+//parent class data members are public
+class Human
+{
+public:
+    string name;
+    int age;
+};
+
+//child class mode of inheritance is public
+class Male: public Human
+{
+public:
+    Male(string n, int a)
+    {
+        name = n;
+        age = a;
+    }
+
+    void display()
+    {
+        cout<<"\nName: "<<name<<"\nAge: "<<age;
+    }
+};
+
+int main()
+{
+    Male m("messi", 35);
+    m.display();
+
+    //output:
+    //
+    //    Name: Messi
+    //    Age: 35
+}
