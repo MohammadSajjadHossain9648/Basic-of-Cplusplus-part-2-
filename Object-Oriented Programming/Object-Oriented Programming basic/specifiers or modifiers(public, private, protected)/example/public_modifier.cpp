d79f4cbc4bff4699befbc038094237d98a94fb80
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-08-14 20:14:28 +0600

    public modifier

--- Object-Oriented Programming/Object-Oriented Programming basic/specifiers or modifiers(public, private, protected)/example/public_modifier.cpp
+++ Object-Oriented Programming/Object-Oriented Programming basic/specifiers or modifiers(public, private, protected)/example/public_modifier.cpp
@@ -1,6 +1,19 @@
 #include<iostream>
 using namespace std;
 /*
+    to know more: https://www.simplilearn.com/tutorials/cpp-tutorial/oops-concepts-in-cpp
+                  https://www.geeksforgeeks.org/object-oriented-programming-in-cpp/
+                  https://www.geeksforgeeks.org/access-modifiers-in-c/
+                  https://www.freecodecamp.org/news/object-oriented-programming-in-c/
+                  https://www.javatpoint.com/cpp-oops-concepts
+                  https://www.programiz.com/cpp-programming/object-class
+                  https://www.educative.io/blog/is-cpp-object-oriented-programming-language
+                  https://www.techtarget.com/searchapparchitecture/definition/object-oriented-programming-OOP
+                  https://www.indeed.com/career-advice/career-development/what-is-object-oriented-programming
+                  https://www.codecademy.com/learn/c-plus-plus-for-programmers/modules/object-oriented-programming-in-cpp/cheatsheet
+                  https://levelup.gitconnected.com/cpp-object-oriented-programming-an-in-depth-guide-f56a597091d8
+*/
+/*
     Public: All the class members declared under the public specifier will be available to everyone. The data
     members and member functions declared as public can be accessed by other classes and functions too. The public
     members of a class can be accessed from anywhere in the program using the direct member access operator (.) with
