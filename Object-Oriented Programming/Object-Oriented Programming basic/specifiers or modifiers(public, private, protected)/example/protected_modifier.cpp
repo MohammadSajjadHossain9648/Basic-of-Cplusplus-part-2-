df11594289d44fdd781850d98d908c0731a67c2c
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-08-20 17:34:36 +0600

    protected modifier

--- Object-Oriented Programming/Object-Oriented Programming basic/specifiers or modifiers(public, private, protected)/example/protected_modifier.cpp
+++ Object-Oriented Programming/Object-Oriented Programming basic/specifiers or modifiers(public, private, protected)/example/protected_modifier.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 using namespace std;
 /*
-    Protected: The protected access modifier is similar to the private access modifier in the sense that it can’t
+    to know more: https://www.simplilearn.com/tutorials/cpp-tutorial/oops-concepts-in-cpp
+                  https://www.geeksforgeeks.org/object-oriented-programming-in-cpp/
+                  https://www.geeksforgeeks.org/access-modifiers-in-c/
+                  https://www.freecodecamp.org/news/object-oriented-programming-in-c/
+                  https://www.javatpoint.com/cpp-oops-concepts
+                  https://www.programiz.com/cpp-programming/object-class
+                  https://www.educative.io/blog/is-cpp-object-oriented-programming-language
+                  https://www.techtarget.com/searchapparchitecture/definition/object-oriented-programming-OOP
+                  https://www.indeed.com/career-advice/career-development/what-is-object-oriented-programming
+                  https://www.codecademy.com/learn/c-plus-plus-for-programmers/modules/object-oriented-programming-in-cpp/cheatsheet
+                  https://levelup.gitconnected.com/cpp-object-oriented-programming-an-in-depth-guide-f56a597091d8
+*/
+/*
+    Protected: The protected access modifier is similar to the private access modifier in the sense that it canâ€™t
     be accessed outside of its class unless with the help of a friend class. The difference is that the class members
     declared as Protected can be accessed by any subclass (derived class) of that class as well.
     Note: This access through inheritance can alter the access modifier of the elements of base class in derived class
