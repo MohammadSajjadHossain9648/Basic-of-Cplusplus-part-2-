caf3819f44a8d9d16a0837446d39037fd0e888df
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-08-21 19:42:17 +0600

    access and change private data members from main function

---
+++ Object-Oriented Programming/Object-Oriented Programming basic/specifiers or modifiers(public, private, protected)/example/access_and_change_private_data_members_from_main_function.cpp
@@ -0,0 +1,90 @@
+#include<iostream>
+using namespace std;
+/*
+    getter use for read/fetch only.
+    setter use for accessing the private data member and for security purpose we can add condition in setter
+    function which we can call encapsulation.
+*/
+/*
+    Why Classes Need Getters and Setters:
+    The convention when designing a C++ class is to make the member variables private to control access to them.
+    With data hiding, you can write code that checks the data coming into a class to make sure it is valid before
+    assigning it to member variables.
+
+    For example, if a class is storing a person’s age, by marking the member variable as private you can provide
+    access to the variable through a function that first checks to make sure the data being passed in is a valid age.
+    If not, you can assign a default value or ask the user to enter the data in again.
+
+    Without this check, the data entered for an age can be any legal value allowed for that type by the compiler.
+    So an age passed to an integer variable can be 34 or it can be 123,345 since those are both valid integer
+    values. A data validating function can stop illegal values from entering the object and preserving the integrity
+    of the data.
+
+    Since a member variable marked as private cannot be accessed in any matter, a class must also provide means for
+    the data stored in a member variable to be retrieved from an object, if that is part of the design. This function
+    is not as important for maintaining data integrity but it is usually a practical necessity.
+
+    Our object-oriented programs can meet these data setting and data retrieval needs by providing getter and setter
+    member functions as part of the class interface.
+*/
+class Voter
+{
+private:
+    string name;
+    int age;
+
+public:
+    //setter
+    void setAge(int a)
+    {
+        age = a;
+    }
+    void setName(string n)
+    {
+        name = n;
+    }
+
+    //getter
+    int getAge()
+    {
+        return age;
+    }
+    string getName()
+    {
+        return name;
+    }
+
+    //display all eligible voter list
+    void display()
+    {
+        cout<<"\n\nname : "<<name<<"\nage: "<<age;
+    }
+};
+
+int main()
+{
+    Voter p1;
+    p1.setAge(20);
+    p1.setName("rafi");
+    cout<<"\n\nname : "<<p1.getName()<<"\nage: "<<p1.getAge();
+
+    /* access name and age together with call:
+            p1.display();
+    */
+
+
+//change age of p1
+    // p1.age = 25; -> cannot access private data member directly
+    p1.setAge(25);
+    cout<<"\n\nafter changing age\nname : "<<p1.getName()<<"\nage: "<<p1.getAge();
+
+
+    //output:
+    //
+    //    name : rafi
+    //    age: 20
+    //
+    //    after changing age
+    //    name : rafi
+    //    age: 25
+}
