1def8b542b929d6d4e16829cd97ac63e6bdef8c4
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-08-23 23:21:51 +0600

    access the Human class

--- Object-Oriented Programming/Object-Oriented Programming basic/use separate class file into another file/access_the_Human_class.cpp
+++ Object-Oriented Programming/Object-Oriented Programming basic/use separate class file into another file/access_the_Human_class.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 #include "class_Human.cpp"
 using namespace std;
-
+/*
+    to know more: https://www.simplilearn.com/tutorials/cpp-tutorial/oops-concepts-in-cpp
+                  https://www.geeksforgeeks.org/object-oriented-programming-in-cpp/
+                  https://www.freecodecamp.org/news/object-oriented-programming-in-c/
+                  https://www.javatpoint.com/cpp-oops-concepts
+                  https://www.programiz.com/cpp-programming/object-class
+                  https://www.educative.io/blog/is-cpp-object-oriented-programming-language
+                  https://www.techtarget.com/searchapparchitecture/definition/object-oriented-programming-OOP
+                  https://www.indeed.com/career-advice/career-development/what-is-object-oriented-programming
+                  https://www.codecademy.com/learn/c-plus-plus-for-programmers/modules/object-oriented-programming-in-cpp/cheatsheet
+                  https://levelup.gitconnected.com/cpp-object-oriented-programming-an-in-depth-guide-f56a597091d8
+*/
 int main()
 {
     //create object h1 of Human class
