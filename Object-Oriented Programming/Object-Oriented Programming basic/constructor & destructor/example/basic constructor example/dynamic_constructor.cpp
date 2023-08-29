2ca5e624380853509b6d5e26171dda0d9a44ca62
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-08-29 19:21:35 +0600

    example of dynamic constructor

---
+++ Object-Oriented Programming/Object-Oriented Programming basic/constructor & destructor/example/basic constructor example/dynamic_constructor.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Person
+{
+private:
+    int* age;
+
+public:
+    Person(int* person_age)
+    {
+        cout<<"Constructor for age is called"<<endl;
+
+        // allocating memory
+        age = new int;
+        age = person_age;
+    }
+
+    // display function to print the class data members value
+    void display()
+    {
+        cout<<"Age of current object: "<<*age<<endl;
+        cout<<endl;
+    }
+
+};
+int main()
+{
+    // creating objects of class using parameterized constructor
+    int age = 25;
+    Person obj1(&age);
+
+    // printing class data members for first object
+    obj1.display();
+
+
+    //output:
+    //    Constructor for age is called
+    //    Age of current object: 25
+}
