f91ae88766df6c60b8dc5f050fbb11b1b666073b
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-09-13 19:53:03 +0600

    take input inside the class constructor

---
+++ Object-Oriented Programming/Object-Oriented Programming basic/constructor & destructor/example/basic constructor example/take_input_inside_the_class_constructor.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+class Student
+{
+    int rno;
+    char name[50];
+    char grade;
+
+public:
+    Student()
+    {
+        cout<<"\nEnter the RollNo: ";
+        cin>>rno;
+
+        cout<<"\nEnter the Name: ";
+        cin>>name;
+
+        cout<<"\nEnter the Grade: ";
+        cin>>grade;
+    }
+
+    //display data
+    void display();
+};
+
+
+void Student::display()
+{
+    cout<<"\nroll no: "<<rno<<"\nname: "<<name<<"\ngrade: "<<grade;
+}
+
+int main()
+{
+    Student s;
+    s.display();
+
+
+    //output:
+    //
+    //    Enter the RollNo: 151
+    //
+    //    Enter the Name: tamim
+    //
+    //    Enter the Grade: A
+    //
+    //    roll no: 151
+    //    name: tamim
+    //    grade: A
+}
