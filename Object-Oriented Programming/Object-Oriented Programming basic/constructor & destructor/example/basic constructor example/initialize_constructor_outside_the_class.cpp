6036f3e509178b4a773b1bf69b9bac0fa92b6f36
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-09-18 21:36:54 +0600

    initialize constructor outside the class

--- Object-Oriented Programming/Object-Oriented Programming basic/constructor & destructor/example/basic constructor example/initialize_constructor_outside_the_class.cpp
+++ Object-Oriented Programming/Object-Oriented Programming basic/constructor & destructor/example/basic constructor example/initialize_constructor_outside_the_class.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 using namespace std;
-
+/*
+    to know more: https://www.naukri.com/learning/articles/constructors-in-c-plus-plus/
+                  https://www.studytonight.com/cpp/constructors-and-destructors-in-cpp.php
+                  https://www.geeksforgeeks.org/constructors-c/
+                  https://www.programiz.com/cpp-programming/constructors
+                  https://www.javatpoint.com/cpp-constructor
+                  http://www.trytoprogram.com/cplusplus-programming/constructors/
+                  https://cplusplus.com/doc/tutorial/classes/
+                  https://www.codementor.io/@supernerdd7/constructor-and-destructor-in-c-1r8kkogm6j
+                  https://levelup.gitconnected.com/learning-c-class-constructors-2aacfd6b7ff7
+*/
 class Car
 {
 public:
