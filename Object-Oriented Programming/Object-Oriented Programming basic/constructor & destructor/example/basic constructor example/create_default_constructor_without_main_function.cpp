9e3df0e90ee170275efa46b226a28f55eef6d8d7
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-09-19 21:54:47 +0600

    create default constructor without main function

---
+++ Object-Oriented Programming/Object-Oriented Programming basic/constructor & destructor/example/basic constructor example/create_default_constructor_without_main_function.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Demo
+{
+    int a, b;
+public:
+    Demo()   // default constructor
+    {
+        cout<<"Default Constructor"<<endl;
+    }
+    Demo(int a, int b):a(a),b(b)  //parameterised constructor
+    {
+        cout<<"parameterized constructor -values"<<a <<" "<< b<<endl;
+    }
+
+} instance;  //instance is a Demo class object
+
+
+int main()
+{
+    //output: Default Constructor
+}
