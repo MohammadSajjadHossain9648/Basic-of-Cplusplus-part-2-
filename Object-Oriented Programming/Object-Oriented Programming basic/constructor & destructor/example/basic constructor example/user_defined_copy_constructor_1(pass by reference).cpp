837b943bd2e121b08a31e728169d5b25008de817
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-09-20 17:57:21 +0600

    user defined copy constructor(pass by reference)

---
+++ Object-Oriented Programming/Object-Oriented Programming basic/constructor & destructor/example/basic constructor example/user_defined_copy_constructor_1(pass by reference).cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+class Rectangle
+{
+private:
+    double length;
+    double width;
+
+public:
+    // parameterized constructor
+    Rectangle(double l, double w)
+    {
+        length = l;
+        width = w;
+    }
+
+    // copy constructor with a Rectangle object as parameter copies data of the obj parameter
+    Rectangle(Rectangle &obj)   //write pass by reference and never write pass by value -> Rectangle(Rectangle obj)
+    {
+        length = obj.length;
+        width = obj.width;
+    }
+
+
+    double calculateArea()
+    {
+        return length * width;
+    }
+};
+
+int main()
+{
+    // create objects to call constructors
+    Rectangle obj1(10,6);
+
+    Rectangle obj2(obj1);   //copy the content using object and can write same as -> Rectangle obj2 = obj1;
+
+    /*whats mean -> Rectangle obj2(obj1);
+    obj2.length = obj1.length;
+    obj2.width = obj1.width;
+    */
+
+
+    //print areas of rectangles
+    cout<<"Area of Rectangle 1: "<<obj1.calculateArea();
+    cout<<"\nArea of Rectangle 2: "<<obj2.calculateArea();
+
+
+    //output:
+    //    Area of Rectangle 1: 60
+    //    Area of Rectangle 2: 60
+}
