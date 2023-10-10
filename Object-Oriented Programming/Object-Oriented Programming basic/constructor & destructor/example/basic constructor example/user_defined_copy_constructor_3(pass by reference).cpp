b496f148a1ccc4d2cb199a990fc88d0539303232
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-10-10 20:27:52 +0600

    user defined copy constructor(pass by reference)

--- Object-Oriented Programming/Object-Oriented Programming basic/constructor & destructor/example/basic constructor example/user_defined_copy_constructor_3(pass by reference).cpp
+++ Object-Oriented Programming/Object-Oriented Programming basic/constructor & destructor/example/basic constructor example/user_defined_copy_constructor_3(pass by reference).cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
-
+/*
+    to know more: https://www.geeksforgeeks.org/constructors-c/
+                  https://www.studytonight.com/cpp/constructors-and-destructors-in-cpp.php
+                  https://www.naukri.com/learning/articles/constructors-in-c-plus-plus/
+                  https://www.programiz.com/cpp-programming/constructors
+                  https://www.scaler.com/topics/cpp/constructor-and-destructor-in-cpp/
+                  https://www.javatpoint.com/cpp-constructor
+                  http://www.trytoprogram.com/cplusplus-programming/constructors/
+                  https://cplusplus.com/doc/tutorial/classes/
+                  https://www.codementor.io/@supernerdd7/constructor-and-destructor-in-c-1r8kkogm6j
+                  https://levelup.gitconnected.com/learning-c-class-constructors-2aacfd6b7ff7
+                  https://www.mygreatlearning.com/blog/constructor-in-cpp/
+*/
 class Employee
 {
 private:
