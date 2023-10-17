0ce8b4bced733b47c4e2bb6f277b5abc3992e7ca
Mohammad Sajjad Hossain <82606819+MohammadSajjadHossain9648@users.noreply.github.com>
2023-10-17 18:29:05 +0600

    example of simple constructor

--- Object-Oriented Programming/Object-Oriented Programming basic/constructor & destructor/example/basic constructor example/simple_constructor.cpp
+++ Object-Oriented Programming/Object-Oriented Programming basic/constructor & destructor/example/basic constructor example/simple_constructor.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 /*
+    to know more: https://www.naukri.com/learning/articles/constructors-in-c-plus-plus/
+                  https://www.studytonight.com/cpp/constructors-and-destructors-in-cpp.php
+                  https://www.geeksforgeeks.org/constructors-c/
+                  https://www.programiz.com/cpp-programming/constructors
+                  https://www.javatpoint.com/cpp-constructor
+                  http://www.trytoprogram.com/cplusplus-programming/constructors/
+                  https://cplusplus.com/doc/tutorial/classes/
+                  https://www.codementor.io/@supernerdd7/constructor-and-destructor-in-c-1r8kkogm6j
+                  https://levelup.gitconnected.com/learning-c-class-constructors-2aacfd6b7ff7
+*/
+/*
     A constructor is a special method that is automatically called when an object of a class is created.
     To create a constructor, use the same name as the class, followed by parentheses (): and it does not
     have any return value.
